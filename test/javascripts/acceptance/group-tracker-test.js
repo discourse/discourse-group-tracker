f964adb1034f2dc3abfc703cc6e800f80df8a649
Jarek Radosz <jradosz@gmail.com>
2022-03-09 14:51:32 +0100

    Fix test

--- test/javascripts/acceptance/group-tracker-test.js
+++ test/javascripts/acceptance/group-tracker-test.js
@@ -1,21 +1,21 @@
 import { acceptance, count } from "discourse/tests/helpers/qunit-helpers";
-import Site from "discourse/models/site";
 import { test } from "qunit";
 import { visit } from "@ember/test-helpers";
 
 acceptance("Group tracker", function (needs) {
   needs.user();
-
-  test("Nav bar item is present", async (assert) => {
-    Site.current().set("tracked_groups", [
+  needs.site({
+    tracked_groups: [
       {
         id: 1,
         add_to_navigation_bar: true,
         name: "testing",
         full_name: "testing",
       },
-    ]);
+    ],
+  });
 
+  test("Nav bar item is present", async function (assert) {
     await visit("/");
 
     assert.strictEqual(
