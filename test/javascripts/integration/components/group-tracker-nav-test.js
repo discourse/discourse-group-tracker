ba5395618ede5665806994c808daefad7e714dcb
Jeff Wong <awole20@gmail.com>
2022-12-15 21:21:31 -0800

    dev: update plugin for glimmer compatibility
    Add plugin outlet connectors to attach pure-glimmer implementations of
    group-tracker buttons and functionality.
    
    This implementation works simultaneously for both widget and glimmer
    implementations of topic timeline.
    
    This still retains all widget attachments - once widgets are removed
    from the topic timeline, we can remove widget references here.
    
    Group tracker nav on mobile view - update to glimmer component

---
+++ test/javascripts/integration/components/group-tracker-nav-test.js
@@ -0,0 +1,28 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import { exists } from "discourse/tests/helpers/qunit-helpers";
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import { getOwner } from "discourse-common/lib/get-owner";
+
+module("Integration | Component | group-tracker-nav", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("It renders", async function (assert) {
+    const store = getOwner(this).lookup("service:store");
+    const topic = store.createRecord("topic", {
+      id: 100,
+      tracked_posts: [
+        { post_number: 2, group: "first" },
+        { post_number: 4, group: "second" },
+      ],
+      title: "Qunit Test Topic",
+    });
+    topic.postStream.set("stream", [100, 200, 300, 500]);
+    topic.set("currentPost", 1);
+    this.set("topic", topic);
+    await render(hbs`<GroupTrackerNav @topic={{this.topic}}/>`);
+
+    assert.ok(exists(".group-tracker-jump-next"));
+  });
+});
