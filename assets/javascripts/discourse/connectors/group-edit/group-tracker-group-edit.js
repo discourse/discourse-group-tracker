a85acab115397d10d8ba9fa1eebe71a8a14976c0
Juan David Martinez <juan@discourse.org>
2024-04-10 13:59:46 -0500

    added checkbox to enable setting and corresponding changes to UI

--- assets/javascripts/discourse/connectors/group-edit/group-tracker-group-edit.js
+++ assets/javascripts/discourse/connectors/group-edit/group-tracker-group-edit.js
@@ -1,26 +1,43 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { ajax } from "discourse/lib/ajax";
+import { inject as service } from "@ember/service";
+export default class GroupTrackerGroupEdit extends Component {
+  @service siteSettings;
+  @tracked group = this.args.outletArgs.group ;
 
-function update(group, name, value) {
-  group.set(name, value);
+  update(group, name, value) {
+    group.set(name, value);
+  
+    return ajax(`/admin/groups/${group.id}/${name}`, {
+      type: "PUT",
+      data: group.getProperties(name),
+    });
+  }
 
-  return ajax(`/admin/groups/${group.id}/${name}`, {
-    type: "PUT",
-    data: group.getProperties(name),
-  });
+  @action
+  trackPostsChanged(value) {
+    this.update(this.group, "track_posts", value);
+  }
+
+  @action
+  trackedPostPriorityGroup(value) {
+    debugger;
+    if (!this.siteSettings.group_tracker_priority_group) {
+      return;
+    }
+
+    this.update(this.group, "track_priority_group", value);
+  }
+
+  @action
+  addToNavigationBarChanged(value) {
+    this.update(this.group, "add_to_navigation_bar", value);
+  }
+
+  @action
+  trackedPostIconChanged(value) {
+    this.update(this.group, "tracked_post_icon", value);
+  }
 }
-
-export default {
-  actions: {
-    trackPostsChanged(value) {
-      update(this.args.group, "track_posts", value);
-    },
-
-    addToNavigationBarChanged(value) {
-      update(this.args.group, "add_to_navigation_bar", value);
-    },
-
-    trackedPostIconChanged(value) {
-      update(this.args.group, "tracked_post_icon", value);
-    },
-  },
-};
