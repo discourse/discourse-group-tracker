46574e5be9accd89306c3aa0ad03f68c113b3e67
Juan David Martinez <juan@discourse.org>
2024-04-11 13:28:44 -0500

    fixed old reference to sitesetting in FE

--- assets/javascripts/discourse/connectors/group-edit/group-tracker-group-edit.js
+++ assets/javascripts/discourse/connectors/group-edit/group-tracker-group-edit.js
@@ -1,10 +1,8 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 import { ajax } from "discourse/lib/ajax";
 export default class GroupTrackerGroupEdit extends Component {
-  @service siteSettings;
   @tracked group = this.args.outletArgs.group;
 
   update(group, name, value) {
@@ -23,10 +21,6 @@ export default class GroupTrackerGroupEdit extends Component {
 
   @action
   trackedPostPriorityGroup(value) {
-    if (!this.siteSettings.group_tracker_priority_group) {
-      return;
-    }
-
     this.update(this.group, "track_posts_with_priority", value);
   }
 
